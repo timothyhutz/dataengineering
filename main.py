85449c4cecc1db392668ab8618cb829d56c686b3
Timothy Hutz <timothyhutz@gmail.com>
2024-01-24 21:17:42 -0700

    started working on sqs client part.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import db
-import logging
+import logging, os
+import boto3
 logging.basicConfig(level=logging.INFO)
 
 def main():
-    pass
+    sqs_endpoint = os.getenv('SQS_ENDPOINT')
+    sqs_client = boto3.client('sqs')
+    # TODO: Add interfaces here request queing for zip code look request, part of the main program.
